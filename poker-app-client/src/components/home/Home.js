3fe3d2e95ff231e53786f7d19083d144e741b8d5
Piotr Majcher <majcherpiotr.dev@gmail.com>
2018-12-26 21:43:46 +0100

    ISSUE #25 home screen new design

--- poker-app-client/src/components/home/Home.js
+++ poker-app-client/src/components/home/Home.js
@@ -2,13 +2,16 @@ import React, { Component } from 'react';
 import { withRouter } from 'react-router-dom';
 import './Home.less';
 import NewGameService from '@services/new-game-service';
+import CardService from '@services/card-service';
 
 class Home extends Component {
 
     constructor(props) {
         super(props);
         this.newGameService = new NewGameService();
+        this.cardService = new CardService();
         this.createNewGame = this.createNewGame.bind(this);
+        this.cardImage = this.cardService.getCardImage("S","A");
     }
 
     createNewGame() {
@@ -20,8 +23,12 @@ class Home extends Component {
     render() {
         return (
         <div className="home">
-            <div className="background-bar">
-                <button className="flat-button-large" onClick={this.createNewGame}>Create new game</button>
+            <div className="background"></div>
+            <div className="columnLeft">
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eu nisl urna. Praesent aliquet ultrices nulla, eget gravida diam ultricies id. Praesent a sollicitudin sapien. Sed ut scelerisque augue. Proin sollicitudin, odio quis egestas varius, sem enim laoreet arcu, ac volutpat nisl magna nec lorem.</p>
+            </div>
+            <div className="columnRight">
+                <img  alt={this.cardImage} src={this.cardImage}/>
             </div>
         </div>
         );
